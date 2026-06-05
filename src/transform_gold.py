3b26656435fc0fd8a452ca429b32df51f2c9a395
daulet <shabakovdaulet@gmail.com>
2026-06-05 23:46:41 +0200

    upgrading layers

--- src/transform_gold.py
+++ src/transform_gold.py
@@ -3,13 +3,15 @@ import pyspark.sql.functions as F
 
 spark = SparkSession.builder.appName("gold").getOrCreate()
 
-df_silver1 = "data/silver/aapl_silver.parquet"
+ticker = "aapl"
+
+df_silver1 = f"data/silver/{ticker}_silver.parquet"
 df_silver = spark.read.parquet(df_silver1)
 
 df_with_year = df_silver.withColumn("Year", F.year("Date"))
 
-df_gold = df_with_year.groupBy("Year").agg(F.avg("Close").alias("Average_Close_Price"), F.sum("Volume").alias("Sum_Of_Volume"))
+df_gold = df_with_year.groupBy("Year").agg(F.round(F.avg("Close"), 2).alias("Average_Close_Price"), F.sum("Volume").alias("Sum_Of_Volume"))
 
 # df_gold.show()
 
-df_gold.write.mode("overwrite").parquet("data/gold/aapl_gold.parquet")
+df_gold.write.mode("overwrite").parquet(f"data/gold/{ticker}_gold.parquet")
