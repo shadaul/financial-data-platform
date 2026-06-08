504f1e1a6b5bcfebd16dad52189681fa2a467e04
daulet <shabakovdaulet@gmail.com>
2026-06-08 20:08:44 +0200

    added interactive plotly chart and expander

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 import streamlit as st
 import sys
 import subprocess
+import pandas as pd
+import plotly.express as px
+import os
 
 zagolovok = st.title("COINER")
 
@@ -14,3 +17,11 @@ if st.button("Run"):
     subprocess.run([sys.executable, "src/transform_gold.py", ticker], check=True)
     st.success("Success", icon="✅")
 
+if ticker:
+    file_path = f"data/silver/{ticker.lower()}_silver.parquet"
+    if os.path.exists(file_path):
+        df = pd.read_parquet(file_path)
+        with st.expander("show the data"):
+            st.dataframe(df)
+        fig = px.line(df, x="date", y="close")
+        st.plotly_chart(fig)
